187057024b1a647e998f30e2475c70dfcf50399f
Ian Buttimer <x14112451@student.ncirl.ie>
2020-08-20 15:11:04 +0100

    MSE and MAAPE for SMA

--- src/main/java/ie/ibuttimer/weather/sma/SmaTableReducer.java
+++ src/main/java/ie/ibuttimer/weather/sma/SmaTableReducer.java
@@ -22,12 +22,12 @@
 
 package ie.ibuttimer.weather.sma;
 
+import ie.ibuttimer.weather.common.AbstractTableReducer;
 import ie.ibuttimer.weather.common.CompositeKey;
 import ie.ibuttimer.weather.common.TimeSeriesData;
 import ie.ibuttimer.weather.misc.Utils;
 import org.apache.hadoop.hbase.client.Mutation;
 import org.apache.hadoop.hbase.client.Put;
-import org.apache.hadoop.hbase.mapreduce.TableReducer;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.Text;
 
@@ -39,22 +39,29 @@ import static ie.ibuttimer.weather.hbase.Hbase.storeValueAsString;
 /**
  * Reducer to perform Simple Moving Average functionality
  */
-public class SmaTableReducer extends TableReducer<CompositeKey, TimeSeriesData, Text>
+public class SmaTableReducer extends AbstractTableReducer<CompositeKey, TimeSeriesData, Text>
                             implements SmaReducerEngine.ISmaReduceOutput<CompositeKey, TimeSeriesData, Text, Mutation> {
 
     // public abstract class TableReducer<KEYIN, VALUEIN, KEYOUT> extends Reducer<KEYIN, VALUEIN, KEYOUT, Mutation>
 
     private SmaReducerEngine<CompositeKey, TimeSeriesData, Text, Mutation> engine;
 
+    private double sqErrorSum;
+    private double absErrorSum;
+
     public static final byte[] ACTUAL = "actual".getBytes();
     public static final byte[] MOVING_AVG = "moving_avg".getBytes();
     public static final byte[] ERROR = "error".getBytes();
     public static final byte[] SQ_ERROR = "sq_error".getBytes();
+    public static final byte[] MSE = "mse".getBytes();
+    public static final byte[] MAAPE = "maape".getBytes();
 
     @Override
     protected void setup(Context context) throws IOException, InterruptedException {
 
         this.engine = new SmaReducerEngine<>(context.getConfiguration(),  this);
+        this.sqErrorSum = 0;
+        this.absErrorSum = 0;
     }
 
     @Override
@@ -62,19 +69,36 @@ public class SmaTableReducer extends TableReducer<CompositeKey, TimeSeriesData, 
 
         engine.reduce(key, values, context);
 
-        // TODO calculate MSE
+        double mse = sqErrorSum/engine.getCount();
+        double mape = absErrorSum/engine.getCount();
+        Put put = new Put(Bytes.toBytes(key.getMainKey()))
+                .addColumn(FAMILY_BYTES, MSE, storeValueAsString(mse))
+                .addColumn(FAMILY_BYTES, MAAPE, storeValueAsString(mape));
+
+        write(context, put);
     }
 
     @Override
     public void reduce(CompositeKey key, String dateTime, double value, double movingAvg, double error, Context context)
                                                                     throws IOException, InterruptedException {
+
+        double sqError = Math.pow(error, 2);
+
         String row = Utils.getRowName(key.getSubKey());
         Put put = new Put(Bytes.toBytes(row))
             .addColumn(FAMILY_BYTES, ACTUAL, storeValueAsString(value))
             .addColumn(FAMILY_BYTES, MOVING_AVG, storeValueAsString(movingAvg))
             .addColumn(FAMILY_BYTES, ERROR, storeValueAsString(error))
-            .addColumn(FAMILY_BYTES, SQ_ERROR, storeValueAsString(Math.pow(error, 2)));
+            .addColumn(FAMILY_BYTES, SQ_ERROR, storeValueAsString(sqError));
 
-        context.write(null, put);
+        sqErrorSum += sqError;
+        double percent;
+        if (error == 0.0 && value == 0.0) {
+            percent = 0;    // special case; 0.0/0.0 = Nan
+        } else {
+            percent = Math.abs(error)/Math.abs(value);
+        }
+        // mean arctangent absolute percentage error (MAAPE); has divide by zero like MAPE
+        absErrorSum += Math.atan(percent);
     }
 }
